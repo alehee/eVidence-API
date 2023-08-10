b0035facb9d9dd0a2926aa2bd3da09ab4b1ce98c
alehee <aleksanderheese@o2.pl>
2023-08-10 12:14:37 +0200

    ef init

---
+++ eVidence-API/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using eVidence_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eVidence_API.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=evidence;uid=evidence;pwd=3Vidence;");
+        }
+    }
+}
--- eVidence-API/Models/Entity.cs
+++ eVidence-API/Models/Entity.cs
@@ -1,8 +1,12 @@
-﻿namespace eVidence_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eVidence_API.Models
 {
     public class Entity
     {
+        [Key]
         public int Id { get; init; }
+        [Required]
         public string Name { get; init; }
     }
 }
