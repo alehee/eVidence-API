33b963257f13c2620d7bed6b4754eae1dad00890
alehee <aleksanderheese@o2.pl>
2023-08-12 19:09:09 +0200

    working on structure controller

--- eVidence-API/Controllers/StructureController.cs
+++ eVidence-API/Controllers/StructureController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using eVidence_API.Models.Context;
 using eVidence_API.Models.Helpers;
 using eVidence_API.Context;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace eVidence_API.Controllers
 {
@@ -16,11 +18,129 @@ namespace eVidence_API.Controllers
             _logger = logger;
         }
 
+        #region Group
+        [HttpPost, Route("group/add")]
+        public Response AddGroup(string name)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                try
+                {
+                    if (context.Groups.Where(a => a.Name == name).Any())
+                        return new Response { Result = "Group with that name already exists" };
+
+                    context.Groups.Add(new Group { Name = name });
+                    context.SaveChanges();
+                    return new Response();
+                } catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"StructureController, {MethodBase.GetCurrentMethod().Name}", MethodBase.GetCurrentMethod().GetParameters());
+                    return new Response { Success = false };
+                }
+            }
+        }
+
+        [HttpGet, Route("group/{id}")]
+        public Response GetGroup(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                try
+                {
+                    var groups = context.Groups.Where(a => a.Id == id);
+
+                    if (!groups.Any())
+                        return new Response { Result = "Group with this id not exists" };
+
+                    return new Response { Result = groups.Single() };
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"StructureController, {MethodBase.GetCurrentMethod().Name}", MethodBase.GetCurrentMethod().GetParameters());
+                    return new Response { Success = false };
+                }
+            }
+        }
+
+        [HttpPost, Route("group/{id}/edit")]
+        public Response EditGroup(int id, string name)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                try
+                {
+                    var groups = context.Groups.Where(a => a.Id == id);
+
+                    if (!groups.Any())
+                        return new Response { Result = "Group with this id not exists" };
+
+                    context.Groups.Where(a => a.Id == id).Single().Name = name;
+                    context.SaveChanges();
+
+                    return new Response();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"StructureController, {MethodBase.GetCurrentMethod().Name}", MethodBase.GetCurrentMethod().GetParameters());
+                    return new Response { Success = false };
+                }
+            }
+        }
+
+        [HttpDelete, Route("group/{id}")]
+        public Response DeleteGroup(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                try
+                {
+                    var groups = context.Groups.Where(a => a.Id == id);
+
+                    if (!groups.Any())
+                        return new Response { Result = "Group with this id not exists" };
+
+                    context.Groups.Remove(groups.Single());
+                    context.SaveChanges();
+
+                    return new Response();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"StructureController, {MethodBase.GetCurrentMethod().Name}", MethodBase.GetCurrentMethod().GetParameters());
+                    return new Response { Success = false };
+                }
+            }
+        }
+        #endregion
+
+        #region Department
+        [HttpPost, Route("department/add")]
+        public Response AddDepartment(string name, int groupId)
+        {
+            // TODO
+            return new Response { Success = false };
+        }
+
         [HttpGet, Route("department/{id}")]
         public Response GetDepartment(int id)
         {
             // TODO
             return new Response { Success = false };
         }
+
+        [HttpPost, Route("department/{id}/edit")]
+        public Response EditDepartment(int id, string name, int groupId)
+        {
+            // TODO
+            return new Response { Success = false };
+        }
+
+        [HttpDelete, Route("department/{id}")]
+        public Response DeleteDepartment(int id)
+        {
+            // TODO
+            return new Response { Success = false };
+        }
+        #endregion
     }
 }
---
+++ eVidence-API/Migrations/20230812154948_creat_init.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace eVidence_API.Migrations
+{
+    public partial class creat_init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Surname = table.Column<string>(type: "longtext", nullable: false),
+                    Keycard = table.Column<string>(type: "longtext", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_DepartmentId",
+                table: "Accounts",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_GroupId",
+                table: "Departments",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
