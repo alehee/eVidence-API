f4755943f6be9ea4551c31e203b95411f14596ed
alehee <aleksanderheese@o2.pl>
2023-12-29 20:06:23 +0100

    un-ignoring

---
+++ eVidence-API/Environment.cs
@@ -0,0 +1,9 @@
+﻿namespace eVidence_API
+{
+        public class Environment
+        {
+                public const string DB_CONNECTION_STRING = "server=localhost;database=evidence;uid=evidence;pwd=3Vidence;";
+                public const string FRONT_ADDRESS = "http://localhost:3000";
+                public const string IP_ADDRESS = "https://localhost:7171";
+        }
+}
